70ea456310ca3c7c50677de5ed84c2783fc928fb
mrmalvicino <mrmalvicino@gmail.com>
2024-01-25 18:44:03 -0300

    Export CSV feature

--- WindowsForms/CustomersForm.cs
+++ WindowsForms/CustomersForm.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using Entities;
 using BLL;
+using System.Configuration;
 
 namespace WindowsForms
 {
@@ -120,6 +122,49 @@ namespace WindowsForms
             }
         }
 
+        private void exportCSV(DataGridView dataGridView, string path)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+                {
+                    // Escribe los encabezados de las columnas
+                    for (int i = 0; i < dataGridView.Columns.Count; i++)
+                    {
+                        sw.Write(dataGridView.Columns[i].HeaderText);
+
+                        if (i < dataGridView.Columns.Count - 1)
+                            sw.Write(",");
+                        else
+                            sw.Write(sw.NewLine);
+                    }
+
+                    // Escribe los datos de cada fila
+                    foreach (DataGridViewRow row in dataGridView.Rows)
+                    {
+                        for (int i = 0; i < dataGridView.Columns.Count; i++)
+                        {
+                            if (row.Cells[i].Value != null)
+                            {
+                                sw.Write(row.Cells[i].Value.ToString());
+                            }
+
+                            if (i < dataGridView.Columns.Count - 1)
+                                sw.Write(",");
+                            else
+                                sw.Write(sw.NewLine);
+                        }
+                    }
+                }
+
+                MessageBox.Show("Exportación exitosa.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar a CSV: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
         // EVENTS
 
         private void CustomersForm_Load(object sender, EventArgs e)
@@ -177,7 +222,7 @@ namespace WindowsForms
 
         private void exportCSVButton_Click(object sender, EventArgs e)
         {
-
+            exportCSV(dataGridView, ConfigurationManager.AppSettings["csv_path"]);
         }
 
         private void filterButton_Click(object sender, EventArgs e)
