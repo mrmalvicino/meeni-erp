44121d5d992638983333b67cd12d221bd740d1fa
mrmalvicino <mrmalvicino@gmail.com>
2025-01-19 00:19:54 -0300

    :rocket: Legal Entities Manager and DAL developed

---
+++ BusinessLogic/LegalEntitiesManager.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess;
+using DomainModel;
+using Exceptions;
+using System;
+using Utilities;
+
+namespace BusinessLogic
+{
+    public class LegalEntitiesManager
+    {
+        private LegalEntity _legalEntity;
+        private LegalEntitiesDAL _legalEntitiesDAL;
+        private ImagesManager _imagesManager;
+        // private AddressesManager _addressesManager;
+
+        public LegalEntitiesManager(Database db)
+        {
+            _legalEntitiesDAL = new LegalEntitiesDAL(db);
+            _imagesManager = new ImagesManager(db);
+            // _addressesManager = new AddressesManager(db);
+        }
+
+        public int Create(LegalEntity legalEntity)
+        {
+            try
+            {
+                return _legalEntitiesDAL.Create(legalEntity);
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessLogicException(ex);
+            }
+        }
+
+        public LegalEntity Read(int legalEntityId)
+        {
+            if (legalEntityId == 0)
+            {
+                return null;
+            }
+
+            try
+            {
+                _legalEntity = _legalEntitiesDAL.Read(legalEntityId);
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessLogicException(ex);
+            }
+
+            _legalEntity.LogoImage = _imagesManager.Read(Helper.GetId(_legalEntity.LogoImage));
+            // _legalEntity.Address = _addressesManager.Read(Helper.GetId(_legalEntity.Address));
+
+            return _legalEntity;
+        }
+    }
+}
---
+++ DataAccess/LegalEntitiesDAL.cs
@@ -0,0 +1,130 @@
+﻿using DomainModel;
+using Exceptions;
+using Utilities;
+using System;
+
+namespace DataAccess
+{
+    public class LegalEntitiesDAL
+    {
+        private Database _db;
+
+        public LegalEntitiesDAL(Database db)
+        {
+            _db = db;
+        }
+
+        public int Create(LegalEntity legalEntity)
+        {
+            try
+            {
+                _db.SetProcedure("sp_create_legal_entity");
+                SetParameters(legalEntity);
+                legalEntity.Id = _db.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw new DataAccessException(ex);
+            }
+            finally
+            {
+                _db.CloseConnection();
+            }
+
+            return legalEntity.Id;
+        }
+
+        public LegalEntity Read(int legalEntityId)
+        {
+            try
+            {
+                _db.SetQuery("select * from legal_entities where legal_entity_id = @legal_entity_id");
+                _db.SetParameter("@legal_entity_id", legalEntityId);
+                _db.ExecuteRead();
+
+                if (_db.Reader.Read())
+                {
+                    LegalEntity legalEntity = new LegalEntity();
+                    ReadRow(legalEntity);
+                    return legalEntity;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new DataAccessException(ex);
+            }
+            finally
+            {
+                _db.CloseConnection();
+            }
+        }
+
+        private void SetParameters(LegalEntity legalEntity, bool isUpdate = false)
+        {
+            if (isUpdate)
+            {
+                _db.SetParameter("@legal_entity_id", legalEntity.Id);
+            }
+
+            if (legalEntity.CUIT != null)
+            {
+                _db.SetParameter("@cuit", legalEntity.CUIT);
+            }
+            else
+            {
+                _db.SetParameter("@cuit", DBNull.Value);
+            }
+
+            _db.SetParameter("@legal_entity_name", legalEntity.Name);
+
+            if (legalEntity.Email != null)
+            {
+                _db.SetParameter("@email", legalEntity.Email);
+            }
+            else
+            {
+                _db.SetParameter("@email", DBNull.Value);
+            }
+
+            if (legalEntity.Phone != null)
+            {
+                _db.SetParameter("@phone", legalEntity.Phone);
+            }
+            else
+            {
+                _db.SetParameter("@phone", DBNull.Value);
+            }
+
+            if (legalEntity.LogoImage != null)
+            {
+                _db.SetParameter("@logo_image_id", legalEntity.LogoImage.Id);
+            }
+            else
+            {
+                _db.SetParameter("@logo_image_id", DBNull.Value);
+            }
+
+            if (legalEntity.Address != null)
+            {
+                _db.SetParameter("@address_id", legalEntity.Address.Id);
+            }
+            else
+            {
+                _db.SetParameter("@address_id", DBNull.Value);
+            }
+        }
+
+        private void ReadRow(LegalEntity legalEntity)
+        {
+            legalEntity.Id = Convert.ToInt32(_db.Reader["legal_entity_id"]);
+            legalEntity.CUIT = _db.Reader["cuit"]?.ToString();
+            legalEntity.Name = _db.Reader["legal_entity_name"].ToString();
+            legalEntity.Email = _db.Reader["email"]?.ToString();
+            legalEntity.Name = _db.Reader["phone"]?.ToString();
+            legalEntity.LogoImage = Helper.InstantiateNull<Image>(_db.Reader["logo_image_id"]);
+            legalEntity.Address = Helper.InstantiateNull<Address>(_db.Reader["address_id"]);
+        }
+    }
+}
